f0e33a16bd4f1e3ac85734166eb3a19caf87ed83
juanmgsierra <47489189+juanmgsierra@users.noreply.github.com>
2021-01-07 23:31:56 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500
+  }
+}));
+
+export default function IndexPage() {
+  const classes = useStyles();
+  const [lpsUSD, setLpsUSD] = useState("");
+  const [lps, setLps] = useState("");
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url:
+        "https://free.currconv.com/api/v7/convert?q=USD_HNL&compact=ultra&apiKey=5a49beefa5e7696bc287"
+    }).then((response) => {
+      setLpsUSD(response.data.USD_HNL);
+    });
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Typography variant="h2" gutterBottom>
+          Cambio del dolar
+        </Typography>
+        <Typography variant="h3">{lpsUSD}</Typography>
+        <br />
+        <Grid item xs={12} sm container>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="lps"
+              variant="outlined"
+              onChange={(e) => {
+                setLps(e.target.value * lpsUSD);
+              }}
+            />
+            <Typography variant="h3">
+              {!lps || lps.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
